21d1c735ebab84ffec3bfc3b06a79b740609196a
Caioestevao100 <caioestevao_@hotmail.com>
2024-09-18 09:23:33 -0300

    criação da database.php, class para configuração e conexão com o DB

---
+++ models/database.php
@@ -0,0 +1,27 @@
+<?php
+
+// Define classe database responsável por gerenciar a cone como o DB
+class Database {
+    //padrão Singleton, cujo o objetivo é garantir que apenas uma única instância de classe seja criada durante a execução do programa, e que essa instância seja reutilzada sempre que necessário
+    private static $instance = null;
+
+    public static function getConnection(){//configuração de conexão como o DB
+        if(!self::$instance){
+            $host = "localhost";
+            $db = "sistemas_usuario";
+            $user = "root";
+            $passaword = "";
+
+            // Conexão usa o drive Mysql (mysql:) e as informações de host e DB
+            self::$instance = new PDO("mysql:$host; dbname=$db", $user, $passaword);//PDO função do php, para conexão com mysql
+
+            //Define modulo de erro para exceções facilitando a depuração e tratamento do erro
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+    } 
+}
+
+?>
+
+criação da database.php, class para configuração e conexão com o DB
